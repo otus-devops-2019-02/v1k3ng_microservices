986824c4553afe1f4bdc480ba256eb14675112ee
Demenev Maksim <6254918+v1k3ng@users.noreply.github.com>
2019-05-22 14:07:27 +0500

    Up to page 11

---
+++ gitlab-ci/terraform/outputs.tf
@@ -0,0 +1,9 @@
+output "Only IP" {
+  value       = "${google_compute_instance.gitlab-ci.network_interface.0.access_config.0.nat_ip}"
+}
+output "HTTP link" {
+  value       = "http://${google_compute_instance.gitlab-ci.network_interface.0.access_config.0.nat_ip}"
+}
+output "SSH link" {
+  value       = "ssh -i ~/.ssh/appuser appuser@${google_compute_instance.gitlab-ci.network_interface.0.access_config.0.nat_ip}"
+}
---
+++ gitlab-ci/terraform/variables.tf
@@ -0,0 +1,16 @@
+variable project {
+  description = "Project ID"
+}
+
+variable public_key_path {
+  # Описание переменной
+  description = "Path to the public key used for ssh access"
+}
+
+variable disk_image {
+  description = "Disk image"
+}
+
+variable private_key_path {
+  description = "Private key"
+}
