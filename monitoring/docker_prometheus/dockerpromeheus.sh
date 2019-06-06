9ead595775bc9d7e157315b8d22f022b60b539af
Demenev Maksim <6254918+v1k3ng@users.noreply.github.com>
2019-06-06 21:33:41 +0500

    From home 06.06

--- monitoring/docker_prometheus/dockerpromeheus.sh
+++ monitoring/docker_prometheus/dockerpromeheus.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 
-echo '{' >> /etc/docker/daemon.json
-echo '  "metrics-addr" : "127.0.0.1:9323",' >> /etc/docker/daemon.json
-echo '  "experimental" : true' >> /etc/docker/daemon.json
-echo '}' >> /etc/docker/daemon.json
+# sudo echo '{' >> /etc/docker/daemon.json
+# sudo echo '  "metrics-addr" : "127.0.0.1:9323",' >> /etc/docker/daemon.json
+# sudo echo '  "experimental" : true' >> /etc/docker/daemon.json
+# sudo echo '}' >> /etc/docker/daemon.json
 
-cp prometheus.yml /tmp/
-chmod 777 /tmp/prometheus.yml
+sudo cp prometheus.yml /tmp/
+sudo chmod 777 /tmp/prometheus.yml
 
-service docker restart
-service docker status
+sudo service docker restart
+# sudo service docker status
 
 /usr/local/bin/docker-compose up -d
 
-#docker run --rm -d -v /tmp/prometheus.yml:/etc/prometheus/prometheus.yml --network host -p 9090:9090 prom/prometheus
-
-#docker run --rm -d --network host -p 3000:3000 -e GF_SECURITY_ADMIN_USER=admin -e GF_SECURITY_ADMIN_PASSWORD=secret grafana/grafana:5.0.0
-
 /usr/local/bin/docker-compose ps
+
+# 70 metrics
--- monitoring/docker_telegraf/dockertelegraf.sh
+++ monitoring/docker_telegraf/dockertelegraf.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
-# echo '{' >> /etc/docker/daemon.json
-# echo '  "metrics-addr" : "127.0.0.1:9323",' >> /etc/docker/daemon.json
-# echo '  "experimental" : true' >> /etc/docker/daemon.json
-# echo '}' >> /etc/docker/daemon.json
+# sudo echo '{' >> /etc/docker/daemon.json
+# sudo echo '  "metrics-addr" : "127.0.0.1:9323",' >> /etc/docker/daemon.json
+# sudo echo '  "experimental" : true' >> /etc/docker/daemon.json
+# sudo echo '}' >> /etc/docker/daemon.json
 
-cp prometheus.yml /tmp/
-chmod 777 /tmp/prometheus.yml
+sudo cp prometheus.yml /tmp/
+sudo chmod 777 /tmp/prometheus.yml
 
-cp telegraf.conf /tmp/
-chmod 777 /tmp/telegraf.conf
+sudo cp telegraf.conf /tmp/
+sudo chmod 777 /tmp/telegraf.conf
 
 
-# service docker restart
+service docker restart
 # service docker status
 
 /usr/local/bin/docker-compose up -d
 /usr/local/bin/docker-compose ps
+
+#209 metrics
